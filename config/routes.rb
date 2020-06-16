cd7c6b157cf51d3260a2ef28395901e62739a59c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-16 07:03:07 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :apartments
   resources :posts, only: [:index, :new, :show, :edit, :update, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/posts', to: 'posts#index'
